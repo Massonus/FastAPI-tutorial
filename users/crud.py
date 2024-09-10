6abc29ec5382c234aadb8ce18ed1fbdf84f01856
Maksym Litvinenko <maksym.lithuanian@gmail.com>
2024-09-10 20:49:28 +0300

    refactor

--- users/crud.py
+++ users/crud.py
@@ -4,7 +4,4 @@ from users.schemas import CreateUser
 def create_user(user_in: CreateUser) -> dict:
     # to return dict
     user = user_in.model_dump()
-    return {
-        "status": "success",
-        "user": user
-    }
+    return {"status": "success", "user": user}
